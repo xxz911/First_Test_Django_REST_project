6cd5c641c4f11b645323c0121a0d9179c38da7e9
xxz <xxz911@rambler.ru>
2022-10-30 00:29:34 +0300

    Created method save, create, delete in class Serializer

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -19,10 +19,36 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+
+        except:
+            return Response({"error": "Method PUT not allowed"})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
+
+    def delete(self, request, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({"error": "Method Delete not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+
+        except:
+            return Response({"error": "Method Delete not allowed"})
+
+        instance.delete()
+        return Response({"post": "delete post" + str(pk)})
