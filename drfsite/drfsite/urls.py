e915bd2a408791c32b0cb15facb9d02874c3b9c7
xxz <xxz911@rambler.ru>
2022-11-04 18:46:32 +0300

    Used SimpleJWT and created JWT-token authentications

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from women.views import *
 # from rest_framework import routers
@@ -30,6 +31,9 @@ urlpatterns = [
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify', TokenVerifyView.as_view(), name='token_verify'),
     # path('api/v1/womenlist', WomenViewSets.as_view({'get': 'list'})),
     # path('api/v1/womenlist/<int:pk>/', WomenViewSets.as_view({'put': 'update'})),
 ]
